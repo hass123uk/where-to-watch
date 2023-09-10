37941d742aa3e4d971fac1dc24e311dd64f4ca9c
Hassan Mahmud <HassanMahmud93@outlook.com>
2023-09-10 17:11:09 +0200

    CRUD on questions with stubs and 1 template for index

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,7 +2,26 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, question_id):
+    return HttpResponse(f"your looking at question {question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f"your looking at results of question {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"your voting on question {question_id}")
